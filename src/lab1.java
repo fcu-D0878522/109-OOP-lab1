4580bae87953ea01406761098e8e667356ffacf5
user <user@EE12>
2020-09-29 09:35:05 +0800

    109-OOP-lab1

---
+++ src/lab1.java
@@ -0,0 +1,11 @@
+
+public class lab1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am ±i®Êºû");
+		System.out.println("My nid is D0878522");
+		System.out.println("This is lab1 Hw");
+	}
+
+}
