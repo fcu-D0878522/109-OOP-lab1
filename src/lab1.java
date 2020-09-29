a4d07b4b95eb1d00f941e69f2494737f7a4fc5a3
fcu-D0878522 <72053299+fcu-D0878522@users.noreply.github.com>
2020-09-29 09:41:13 +0800

    Update lab1.java

--- src/lab1.java
+++ src/lab1.java
@@ -3,7 +3,7 @@ public class lab1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am ");
+		System.out.println("I am jin wei");
 		System.out.println("My nid is D0878522");
 		System.out.println("This is lab1 Hw");
 	}
